c7580d7a62e8d7f130dba5f59077d461f4bf7993
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-24 21:32:24 +0300

    Useeffect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 function App() {
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }); 
 
   console.log("Component rendering");
 
